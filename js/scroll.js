9d860625716063a82520b37932c5a2f3cd26ae35
Your Nam <Jpsilvabarr@gmail.com>
2023-03-09 14:18:21 -0300

    1.0
    Feedback da pagina ao fazer scroll no menu

--- js/scroll.js
+++ js/scroll.js
@@ -8,5 +8,23 @@ function menuView(){
 }
 window.onscroll = function () {
     menuView();
+    if ((document.body.scrollTop > document.getElementById("sobre").getBoundingClientRect().y-2 || document.documentElement.scrollTop > document.getElementById("sobre").getBoundingClientRect().y-2) && 
+    (document.body.scrollTop < document.getElementById("projetos").getBoundingClientRect().y || document.documentElement.scrollTop < document.getElementById("projetos").getBoundingClientRect().y) ){
+        document.getElementById('linkSobre').style = 'color: #e25f71; pointer-events: none;';
+        document.getElementById('linkProj').style = 'color: #f4f4f4; pointer-events: all;';
+        document.getElementById('linkHab').style = 'color: #f4f4f4; pointer-events: all;';
+    }else if ((document.body.scrollTop > document.getElementById("projetos").getBoundingClientRect().y || document.documentElement.scrollTop > document.getElementById("projetos").getBoundingClientRect().y) &&
+    (document.body.scrollTop < document.getElementById("habilidades").getBoundingClientRect().y || document.documentElement.scrollTop < document.getElementById("habilidades").getBoundingClientRect().y)){
+        document.getElementById('linkProj').style = 'color: #e25f71; pointer-events: none;';
+
+        document.getElementById('linkSobre').style = 'color: #f4f4f4; pointer-events: all;';
+        document.getElementById('linkHab').style = 'color: #f4f4f4; pointer-events: all;';
+
+    }else{
+        document.getElementById('linkHab').style = 'color: #e25f71; pointer-events: none;';
+
+        document.getElementById('linkSobre').style = 'color: #f4f4f4; pointer-events: all;';
+        document.getElementById('linkProj').style = 'color: #f4f4f4; pointer-events: all;';
+    }
 }
 menuView();
